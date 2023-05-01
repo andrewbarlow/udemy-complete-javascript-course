ec110cf33c83cd6d744ab5bf19c0c1a71bad5ed7
Andrew Barlow <andrew.barlow@uwaterloo.ca>
2023-05-01 19:57:50 -0400

    started JS course

---
+++ 01-Fundamentals-Part-1/starter/assignment1.js
@@ -0,0 +1,17 @@
+let country = "Canada";
+let continent = "North America";
+let population = "30";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+const isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+language = "English";
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,31 @@
+/*
+let javascriptIsFun = true;
+
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+
+javascriptIsFun = "yes";
+
+console.log(javascriptIsFun);
+*/
+let currentYear = 2023;
+const ageAndrew = currentYear - 2000;
+const randomTime = currentYear - 1029;
+console.log(ageAndrew);
+console.log(randomTime);
+
+console.log(ageAndrew * 2, ageAndrew / 2, 2 ** 3);
+
+const firstName = "Andrew";
+const lastName = "Barlow";
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5;
+x += 10;
+console.log(x);
+
+console.log(ageAndrew > randomTime);
+console.log(ageAndrew >= 18);
+
+const isFullAge = ageAndrew >= 18;
