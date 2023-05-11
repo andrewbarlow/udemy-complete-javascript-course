2f6b2e41d8d91cccbcd3d18492e9f178517d1f9c
Andrew Barlow <andrew.barlow@uwaterloo.ca>
2023-05-10 22:31:35 -0400

    pig game

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -3,9 +3,7 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -23,3 +21,9 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
