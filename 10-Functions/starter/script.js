d90fdfbabf1132477addf43451ced9a0f38f0ccb
Andrew Barlow <andrew.barlow@uwaterloo.ca>
2023-05-22 23:24:44 -0400

    working on higher order functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,104 @@
 'use strict';
+
+// const bookings = [];
+
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   // ES5
+//   // numPassengers = numPassengers || 1;
+//   // price = price || 199;
+
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 3);
+
+const flight = 'LH234';
+const andrew = {
+  name: 'Andrew Barlow',
+  passport: 12345,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 12345) {
+    // alert('Check in');
+    console.log('Check in');
+  } else {
+    // alert('Wrong passport');
+    console.log('Wrong passport');
+  }
+};
+
+checkIn(flight, andrew);
+console.log(flight);
+console.log(andrew);
+
+// Is the same as doing...
+const flightNum = flight;
+const passenger = andrew;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+
+newPassport(andrew);
+console.log(andrew);
+checkIn(flight, andrew);
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('wave');
+};
+
+document.body.addEventListener('click', high5);
+
+['Andrew', 'Martha', 'Adam'].forEach(high5);
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greetHey = greet('Hey');
+
+greetHey('Andrew');
+greetHey('Steven');
+
+greet('Hello')('Andrew');
+
+const greet2 = () => {};
