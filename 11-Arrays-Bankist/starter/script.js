f15532d9f292f46c26219174ca15dceafdadb0b8
Andrew Barlow <andrew.barlow@uwaterloo.ca>
2023-05-27 22:53:20 -0400

    map filter reduce

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,16 +61,192 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type} </div>
+      <div class="movements__value"> ${mov}€</div>
+    </div>
+  `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
+
+const createUsernames = function (accts) {
+  accts.forEach(function (acct) {
+    acct.username = acct.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+const user = 'Steven Thomas Williams';
+
+createUsernames(accounts);
+console.log(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// // SLICE
+// // console.log(arr.slice(2));
+// // console.log(arr.slice(2, 4));
+// // console.log(arr.slice(-2));
+// // console.log(arr.slice(-1));
+// // console.log(arr.slice(1, -2));
+// // console.log(arr.slice());
+// // console.log(arr.slice([...arr]));
+
+// // SPLICE
+// // console.log(arr.splice(2));
+// console.log(arr);
+// arr.splice(-1);
+// arr.splice(1, 2);
+// console.log(arr);
+
+// // REVERSE
+// arr = ['a', 'b', 'c', 'd', 'e'];
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse());
+// console.log(arr2);
+
+// // CONCAT
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// console.log([...arr, ...arr2]);
+
+// // JOIN
+// console.log(letters.join(' - '));
+
+// const arr = [23, 11, 64];
+
+// console.log(arr[0]);
+// console.log(arr.at(0));
+
+// console.log(arr[arr.length - 1]);
+// console.log(arr.at(-1));
+
+// console.log('jonas'.at(0));
+
+// const movements = [200, 400, -400, 3000, -650, -130, 70, 1300];
+
+// for (const [i, movement] of movements.entries()) {
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+//   }
+// }
+
+// console.log('---- FOREACH ----');
+// movements.forEach(functiofn (mov, i, arr) {
+//   if (mov > 0) {
+//     console.log(`Movement ${i + 1}: You deposited ${mov}`);
+//   } else {
+//     console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+//   }
+// });
+
+// 0: funtion(200)
+// 1: function(450)
+// 2: function(400)
+
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+// currencies.forEach(function (value, key, map) {
+//   console.log(`${key}: ${value}`);
+// });
+
+// // Set
+// const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+
+// console.log(currenciesUnique);
+
+// currenciesUnique.forEach(function (value, _, map) {
+//   console.log(`${value}: ${value}`);
+// });
+
+// CODING CHALLENGE 1
+// const JuliaArr = [3, 5, 2, 12, 7];
+// const KateArr = [4, 1, 15, 8, 3];
+
+// const checkDogs = function (arr1, arr2) {
+//   const correctedArr = arr1;
+//   correctedArr.splice(-2);
+//   const concatArr = correctedArr.concat(arr2);
+
+//   concatArr.forEach(function (dogAge) {
+//     const isAdult =
+//       dogAge >= 3
+//         ? `The ${dogAge} year old dog is an adult`
+//         : `The ${dogAge} year old dog is a puppy`;
+//     console.log(isAdult);
+//   });
+// };
+
+// checkDogs(JuliaArr, KateArr);
+
+// const eurToUsd = 1.1;
+
+// const movementsUSD = movements.map(mov => {
+//   return mov * eurToUsd;
+// });
+
+// console.log(movements);
+// console.log(movementsUSD);
+
+// const movementsUSDFor = [];
+
+// for (const mov of movements) {
+//   movementsUSDFor.push(mov * eurToUsd);
+// }
+
+// const movementsDescriptions = movements.map(
+//   (mov, i) =>
+//     `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+//       mov
+//     )}`
+// );
+
+// console.log(movementsDescriptions);
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements)
+  if (mov > 0) {
+    depositsFor.push(mov);
+  }
+
+console.log(depositsFor);
+
+const withdrawls = movements.filter(function (mov) {
+  return mov < 0;
+});
